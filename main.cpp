3f2175d75de06e8cc8426a87d28846737c874687
frnteq <francisco.tequida00@gmail.com>
2017-12-27 00:16:20 -0700

    actualizacion main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include "arbol_bp.h"
-
+#include "funciones_extra.h"
 using namespace std;
 
 int main(){
     Arbol_BP * A = new Arbol_BP();
-
+    ///escribir_en_archivo();
+    tomar_valores_de_archivo(A);
     /**for(int i = 1000 ; i >= 1 ; i--){
         A->agregar(i);
     }**/
-    for(int i = 1 ; i <= 1000 ; i++){
+    /**for(int i = 1 ; i <= 1000 ; i++){
         A->agregar(i);
-    }
+    }**/
 
     A->pintar();
     return 0;
